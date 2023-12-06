6c23f9c77c3d9aed9d30613f4ec8e5be1abd3cfe
smart <smart@smhrd>
2023-12-06 16:08:14 +0900

    html -> jsp로 수정 (유빈)

--- helpdog/src/main/java1/com/smhrd/controller/loginCon.java
+++ helpdog/src/main/java1/com/smhrd/controller/loginCon.java
@@ -45,7 +45,7 @@ public class loginCon extends HttpServlet {
       }else {
     	  System.out.println("로그인 실패 ㅠㅠ ");
     	  // 로그인 실패하면 login.html로 이동
-    	  response.sendRedirect("login.html");
+    	  response.sendRedirect("login.jsp");
       }
    }
 
